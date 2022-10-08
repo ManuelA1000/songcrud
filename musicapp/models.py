997b93f271e15b7f4142d72649e7225cec1b0e47
ManuelA1000 <enriquezmanuel012@gmail.com>
2022-10-08 15:22:21 +0300

    updated models

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,4 +1,6 @@
+import django.utils.timezone
 from django.db import models
+
 
 # Create your models here.
 # Artiste, Song, Lyric
@@ -11,16 +13,19 @@ from django.db import models
 class Artiste(models.Model):
     first_name = models.CharField(max_length=400)
     last_name = models.CharField(max_length=400)
-    age = models.IntegerField(max_length='')
+    age = models.IntegerField()
 
 
 class Song(models.Model):
+    # ForeignKey attribute for specifying many-to-one relationship
+    Artiste = models.ForeignKey(Artiste, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    date_released = models.DateField()
+    date_released = models.DateField(default=django.utils.timezone.now())
     likes = models.IntegerField()
     artiste_id = models.CharField(max_length=10)
 
 
 class Lyric(models.Model):
+    Song = models.ForeignKey(Song, on_delete=models.CASCADE)
     content = models.TextField()
     song_id = models.IntegerField()
